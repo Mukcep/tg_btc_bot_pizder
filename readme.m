9f2b3805d0c17a9298ee9bfbb574eb24dd7a3911
Maksim P <wmg.developer@gmail.com>
2021-10-01 21:46:29 +0300

    Добавлено ридми

---
+++ readme.m
@@ -0,0 +1,14 @@
+Для запуска сначала создать окружение:
+python3 -m venv env
+
+Затем активировать его:
+На винде: env\Scripts\Activate
+на лине: source env/bin activate
+
+Если все ок то в строке cmd добавится (env) вначале
+
+Далее установить зависимости:
+
+$ (env) [b]pip install -r requirements.txt[/b]
+
+После этого отредачить app.py заполнив api_id, api_hash и никнейм бота
