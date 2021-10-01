662435b9fee08debfecd7d64c0f1810dd25c6b69
Maksim P <wmg.developer@gmail.com>
2021-10-01 21:57:10 +0300

    Добавил етх бота и чатекс бота

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import re
 app_api_id = ''
 app_api_hash = ''
 tg_btc_bot_username = ''
-
+tg_eth_bot_username = ''
+tg_chatex_bot_username = ''
 client = telethon.TelegramClient('Pizder', app_api_id, app_api_hash)
 client.start()
 
@@ -15,5 +16,11 @@ async def handler(event):
   if re.search(r'BTC_CHANGE_BOT\?start=', message):
     m = re.search(r'c_\S+', message)
     await client.send_message(tg_btc_bot_username, '/start ' + m.group(0).replace('`', ''))
+  if re.search(r'ETH_CHANGE_BOT\?start=', message):
+    m = re.search(r'c_\S+', message)
+    await client.send_message(tg_eth_bot_username, '/start ' + m.group(0).replace('`', ''))
+  if re.search(r'Chatex_bot\?start=', message):
+    m = re.search(r'c_\S+', message)
+    await client.send_message(tg_chatex_bot_username, '/start ' + m.group(0).replace('`', ''))
 
 client.run_until_disconnected()
